f6de2724946d2698407225a37c9926430573d1f0
Tom Gauden <tgauden@outlook.com>
2020-10-18 10:47:27 -0400

    Adding new models

---
+++ Shared/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBattle2.Shared
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int HitPoints { get; set; }
+        public int BananaCost { get; set; }
+    }
+}
---
+++ Shared/UserUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBattle2.Shared
+{
+    public class UserUnit
+    {
+        public int UserId { get; set; }
+        public int UnitId { get; set; }
+        public int HitPoints { get; set; }
+    }
+}
